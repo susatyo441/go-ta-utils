b1a0b0ce0e6839a48cbff801cb374e17eb9c1a37
susatyo441 <susatyo441@gmail.com>
2025-03-05 15:29:42 +0700

    fix: update transaction model

--- model/transaction_model.go
+++ model/transaction_model.go
@@ -6,14 +6,19 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type TransactionProductAttribute struct {
+	ID       primitive.ObjectID `json:"_id,omitempty"        bson:"_id,omitempty"`
+	Name     string             `json:"name"            bson:"name"`
+	Price    int                `json:"price"          bson:"price"`
+	Category AttributeEmbedded  `json:"category"            bson:"category"`
+	Quantity int                `json:"quantity"          bson:"quantity"`
+}
+
 type Transaction struct {
-	ID         primitive.ObjectID `json:"_id,omitempty"        bson:"_id,omitempty"`
-	Product    AttributeEmbedded  `json:"product"            bson:"product"`
-	Category   AttributeEmbedded  `json:"category"            bson:"category"`
-	Quantity   int                `json:"quantity"          bson:"quantity"`
-	Price      int                `json:"price"          bson:"price"`
-	TotalPrice int                `json:"totalPrice"          bson:"totalPrice"`
-	StoreID    primitive.ObjectID `json:"storeId"        bson:"storeId"`
+	ID         primitive.ObjectID            `json:"_id,omitempty"        bson:"_id,omitempty"`
+	Products   []TransactionProductAttribute `json:"products"            bson:"products"`
+	TotalPrice int                           `json:"totalPrice"          bson:"totalPrice"`
+	StoreID    primitive.ObjectID            `json:"storeId"        bson:"storeId"`
 
 	CreatedAt time.Time `json:"createdAt"            bson:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"            bson:"updatedAt"`
