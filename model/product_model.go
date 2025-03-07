e3d920ec6a7458747804f32bac45caa2e9cd7775
susatyo441 <susatyo441@gmail.com>
2025-03-07 13:11:05 +0700

    fix: add variations to product

--- model/product_model.go
+++ model/product_model.go
@@ -6,15 +6,23 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type ProductVariationAttr struct {
+	Name         string `json:"name" bson:"name"`
+	Price        int    `json:"price" bson:"price"`
+	CapitalPrice *int   `json:"capitalPrice" bson:"capitalPrice"`
+	Stock        int    `json:"stock" bson:"stock"`
+}
+
 type Product struct {
-	ID         primitive.ObjectID `json:"_id,omitempty"        bson:"_id,omitempty"`
-	Name       string             `json:"name"            bson:"name"`
-	Category   AttributeEmbedded  `json:"category"            bson:"category"`
-	CoverPhoto string             `json:"coverPhoto" bson:"coverPhoto"`
-	Stock      int                `json:"stock" bson:"stock"`
-	Price      int                `json:"price" bson:"price"`
-	StoreID    primitive.ObjectID `json:"storeId"        bson:"storeId"`
-	CapitalPrice *int			  `json:"capitalPrice" bson:"capitalPrice"`
+	ID           primitive.ObjectID     `json:"_id,omitempty"        bson:"_id,omitempty"`
+	Name         string                 `json:"name"            bson:"name"`
+	Category     AttributeEmbedded      `json:"category"            bson:"category"`
+	CoverPhoto   string                 `json:"coverPhoto" bson:"coverPhoto"`
+	Stock        *int                   `json:"stock" bson:"stock"`
+	Price        *int                   `json:"price" bson:"price"`
+	StoreID      primitive.ObjectID     `json:"storeId"        bson:"storeId"`
+	CapitalPrice *int                   `json:"capitalPrice" bson:"capitalPrice"`
+	Variations   []ProductVariationAttr `json:"variations" bson:"variations"`
 
 	CreatedAt time.Time `json:"createdAt"            bson:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"            bson:"updatedAt"`
