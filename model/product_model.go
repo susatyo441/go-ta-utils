5414e309992a4103fb98c628aa30ab3816dfba19
susatyo441 <susatyo441@gmail.com>
2025-03-07 13:15:29 +0700

    fix: rename vairations to variants

--- model/product_model.go
+++ model/product_model.go
@@ -6,7 +6,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type ProductVariationAttr struct {
+type ProductVariantsAttr struct {
 	Name         string `json:"name" bson:"name"`
 	Price        int    `json:"price" bson:"price"`
 	CapitalPrice *int   `json:"capitalPrice" bson:"capitalPrice"`
@@ -14,15 +14,15 @@ type ProductVariationAttr struct {
 }
 
 type Product struct {
-	ID           primitive.ObjectID     `json:"_id,omitempty"        bson:"_id,omitempty"`
-	Name         string                 `json:"name"            bson:"name"`
-	Category     AttributeEmbedded      `json:"category"            bson:"category"`
-	CoverPhoto   string                 `json:"coverPhoto" bson:"coverPhoto"`
-	Stock        *int                   `json:"stock" bson:"stock"`
-	Price        *int                   `json:"price" bson:"price"`
-	StoreID      primitive.ObjectID     `json:"storeId"        bson:"storeId"`
-	CapitalPrice *int                   `json:"capitalPrice" bson:"capitalPrice"`
-	Variations   []ProductVariationAttr `json:"variations" bson:"variations"`
+	ID           primitive.ObjectID    `json:"_id,omitempty"        bson:"_id,omitempty"`
+	Name         string                `json:"name"            bson:"name"`
+	Category     AttributeEmbedded     `json:"category"            bson:"category"`
+	CoverPhoto   string                `json:"coverPhoto" bson:"coverPhoto"`
+	Stock        *int                  `json:"stock" bson:"stock"`
+	Price        *int                  `json:"price" bson:"price"`
+	StoreID      primitive.ObjectID    `json:"storeId"        bson:"storeId"`
+	CapitalPrice *int                  `json:"capitalPrice" bson:"capitalPrice"`
+	Variants     []ProductVariantsAttr `json:"variants" bson:"variants"`
 
 	CreatedAt time.Time `json:"createdAt"            bson:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"            bson:"updatedAt"`
