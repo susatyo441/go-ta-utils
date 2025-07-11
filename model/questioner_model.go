9576a1c703661c3d5bc097cf2cc6b2a6635f4621
susatyo441 <susatyo441@gmail.com>
2025-07-11 15:30:31 +0700

    feat: question 7-10

--- model/questioner_model.go
+++ model/questioner_model.go
@@ -15,6 +15,10 @@ type Questioner struct {
 	Question4 int                `json:"question4"            bson:"question4"`
 	Question5 int                `json:"question5"            bson:"question5"`
 	Question6 int                `json:"question6"            bson:"question6"`
+	Question7 int                `json:"question7"            bson:"question7"`
+	Question8 int                `json:"question8"            bson:"question8"`
+	Question9 int                `json:"question9"            bson:"question9"`
+	Question10 int                `json:"question10"            bson:"question10"`
 
 	CreatedAt time.Time `json:"createdAt"            bson:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"            bson:"updatedAt"`
